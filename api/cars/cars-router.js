19a9f1568106fac2fdb9b0be85a506d716acf0c9
Pauline122112 <pauline.design007@gmail.com>
2021-10-05 22:36:43 -0500

    trying to make this work

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,13 +1,44 @@
-const router = require("express").Router();
+const express = require("express");
+const {
+	checkCarId,
+	checkCarPayload,
+	checkVinNumberUnique,
+	checkVinNumberValid,
+} = require("./cars-middleware");
 const Cars = require("./cars-model");
+
+const router = express.Router();
 
 router.get("/", async (req, res, next) => {
 	try {
-		const allCars = await Cars.getAll();
-		res.json(allCars);
-	} catch (err) {
-		next(err);
+		const cars = await Cars.getAll();
+		res.status(200).json(cars);
+	} catch (error) {
+		next(error);
 	}
 });
+
+router.get("/:id", checkCarId, async (req, res, next) => {
+	try {
+		res.json(req.car);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.post(
+	"/",
+	checkCarPayload,
+	checkVinNumberValid,
+	checkVinNumberUnique,
+	async (req, res, next) => {
+		try {
+			const possibleCar = await Cars.create(req.body);
+			res.status(201).json(possibleCar);
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,19 +1,17 @@
-const express = require("express")
-const helmet = require('helmet')
+const express = require("express");
+const carsRouter = require("./cars/cars-router.js");
+const server = express();
 
-const carsRouter = require('./cars/cars-router')
+server.use(express.json());
 
-const server = express()
+server.use("/api/cars", carsRouter);
 
-server.use(express.json())
-server.use('/api/cars', carsRouter)
-server.use(helmet())
-
-server.use("*", (req, res) => {
-	res.status(404).json({
-		message: "not found",
+server.use((err, req, res, next) => {
+	//eslint-disable-line
+	res.status(err.status || 500).json({
+		message: err.message,
+		stack: err.stack,
 	});
 });
 
-
-module.exports = server
+module.exports = server;
